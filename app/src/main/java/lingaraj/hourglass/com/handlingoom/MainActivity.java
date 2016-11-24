3a251f38380aaafef4ceb2b368847a78b49c45af
Lingaraj Sankaravelu <lingaraj.s@hotmail.com>
2016-11-24 07:03:12 +0530

    initial commit

---
+++ app/src/main/java/lingaraj/hourglass/com/handlingoom/MainActivity.java
@@ -0,0 +1,13 @@
+package lingaraj.hourglass.com.handlingoom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
